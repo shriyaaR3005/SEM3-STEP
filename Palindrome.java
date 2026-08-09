460dc6c528b9609daeef0a944fab550c0a3ec730
shriyaaR3005 <sr4146@srmist.edu.in>
2026-08-09 09:40:43 +0530

    class problems done

---
+++ Palindrome.java
@@ -0,0 +1,57 @@
+import java.util.*;
+
+public class Palindrome {
+    static boolean isPalindromeIterative(String text) {
+        int left = 0, right = text.length() - 1;
+
+        while (left < right) {
+            if (text.charAt(left) != text.charAt(right))
+                return false;
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+
+    static boolean isPalindromeRecursive(String text) {
+        if (text.length() <= 1)
+            return true;
+
+        if (text.charAt(0) != text.charAt(text.length() - 1))
+            return false;
+
+        return isPalindromeRecursive(text.substring(1, text.length() - 1));
+    }
+
+    static boolean isPalindromeArrayReversal(String text) {
+        char[] original = text.toCharArray();
+        char[] reversed = text.toCharArray();
+
+        int left = 0, right = reversed.length - 1;
+
+        while (left < right) {
+            char temp = reversed[left];
+            reversed[left] = reversed[right];
+            reversed[right] = temp;
+            left++;
+            right--;
+        }
+
+        return Arrays.equals(original, reversed);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String text = sc.nextLine();
+
+        System.out.println("Iterative: " +
+                (isPalindromeIterative(text) ? "Palindrome" : "Not Palindrome"));
+
+        System.out.println("Recursive: " +
+                (isPalindromeRecursive(text) ? "Palindrome" : "Not Palindrome"));
+
+        System.out.println("Array Reversal: " +
+                (isPalindromeArrayReversal(text) ? "Palindrome" : "Not Palindrome"));
+    }
+}
